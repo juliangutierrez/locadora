74f506eb522c6d131bab9831f9108a2be4901a17
Julian Gutierrez <julian.gutierrez@uniriotec.br>
2012-05-17 15:03:26 -0300

    Criada lógica de controle de quantidade de filmes alugados

--- app/models/filme.rb
+++ app/models/filme.rb
@@ -1,7 +1,7 @@
 class Filme < ActiveRecord::Base
-	validates :nome,  :presence => true
-	validates  :genero,  :presence => true
-	validates :qtde, :presence => true
+	validates :nome, :presence => true
+	validates  :genero, :presence => true
+	validates :qtde, :presence => true, :numericality => { :only_integer => true, :greater_than => -1}
 	
 	belongs_to :genero
 	has_and_belongs_to_many :alugueis
--- db/migrate/20120517135000_add_qtde_to_filmes.rb
+++ db/migrate/20120517135000_add_qtde_to_filmes.rb
@@ -1,6 +1,6 @@
 class AddQtdeToFilmes < ActiveRecord::Migration
   def change
     add_column :filmes, :qtde, :integer
-
+	Filme.update_all ["qtde = ?", 1]
   end
 end
